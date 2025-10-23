3712b7dd0255f1adeff4b7eab851ebc900ffc8f6
mikhaelesa <mikeul2.gt@gmail.com>
2025-10-24 01:31:05 +0700

    (feat): Add isActive state to TextInput

--- src/components/organisms/TextInput/TextInput.component.tsx
+++ src/components/organisms/TextInput/TextInput.component.tsx
@@ -1,19 +1,22 @@
 import InputBase from "@/src/components/molecules/InputBase";
-import { ComponentProps } from "react";
+import { ComponentProps, useState } from "react";
 
 interface ITextInputProps extends ComponentProps<typeof InputBase> {
   placeholder?: string;
 }
 
 const TextInput = ({ placeholder, ...props }: ITextInputProps) => {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <InputBase {...props}>
+    <InputBase isActive={isActive} {...props}>
       <input
         className="outline-none w-full text-m"
         type="text"
         placeholder={placeholder}
         required={props.isRequired}
         disabled={props.isDisabled}
+        onFocus={() => setIsActive(true)}
+        onBlur={() => setIsActive(false)}
       />
     </InputBase>
   );
