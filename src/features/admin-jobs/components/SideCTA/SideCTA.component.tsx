92e812911e58241c1921e7e668cf7276dd6074a7
mikhaelesa <mikeul2.gt@gmail.com>
2025-10-24 01:32:38 +0700

    (feat): Add job list and SideCTA in AdminJobs page. Added searchbar

---
+++ src/features/admin-jobs/components/SideCTA/SideCTA.component.tsx
@@ -0,0 +1,32 @@
+import Button from "@/src/components/atoms/Button";
+import Image from "next/image";
+
+const SideCTA = () => {
+  return (
+    <aside>
+      <div className="flex flex-col gap-y-6 p-6 rounded-2xl relative overflow-hidden before:absolute before:inset-0 before:bg-black before:opacity-[0.72] before:z-10">
+        <div className="overflow-hidden absolute inset-0">
+          <Image
+            src="/images/bg-create-job.jpg"
+            width={300}
+            height={300}
+            alt="Background"
+          />
+        </div>
+        <div className="flex flex-col gap-y-1 z-10">
+          <p className="whitespace-nowrap text-neutral-40 text-xl font-bold">
+            Recruit the best candidates
+          </p>
+          <p className="text-neutral-10 text-m font-bold">
+            Create jobs, invite, and hire with ease
+          </p>
+        </div>
+        <Button size="large" className="z-10">
+          Create a new job
+        </Button>
+      </div>
+    </aside>
+  );
+};
+
+export default SideCTA;
--- src/features/admin-jobs/view/AdminJobs.view.tsx
+++ src/features/admin-jobs/view/AdminJobs.view.tsx
@@ -1,14 +1,42 @@
+"use client";
+
+import IcSearch from "@/src/components/atoms/Icons/IcSearch.component";
 import ErrorNoJobs from "@/src/components/molecules/ErrorNoJobs";
 import Navbar from "@/src/components/molecules/Navbar";
+import TextInput from "@/src/components/organisms/TextInput";
 import ErrorBoundary from "@/src/components/templates/ErrorBoundary";
+import AdminJobCard from "../components/AdminJobCard";
+import SideCTA from "../components/SideCTA";
 
 const AdminJobs = () => {
   return (
     <>
       <Navbar />
-      <main>
-        <ErrorBoundary isError errorComponent={<ErrorNoJobs />}></ErrorBoundary>
-      </main>
+      <ErrorBoundary errorComponent={<ErrorNoJobs />}>
+        <main className="relative">
+          <div className="flex gap-x-6 pt-9 px-6">
+            <div className="w-full flex flex-col gap-y-4">
+              <TextInput
+                placeholder="Search by job details"
+                iconRight={<IcSearch className="text-primary-main" />}
+              />
+              <div className="flex flex-col gap-y-4">
+                <AdminJobCard />
+                <AdminJobCard />
+                <AdminJobCard />
+                <AdminJobCard />
+                <AdminJobCard />
+                <AdminJobCard />
+                <AdminJobCard />
+                <AdminJobCard />
+                <AdminJobCard />
+                <AdminJobCard />
+              </div>
+            </div>
+            <SideCTA />
+          </div>
+        </main>
+      </ErrorBoundary>
     </>
   );
 };
