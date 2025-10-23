5f544a73ba8a2491aff517db49b04bff1d51e5d1
mikhaelesa <mikeul2.gt@gmail.com>
2025-10-23 21:23:43 +0700

    Initialize project

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import { clsx } from "clsx";
+import { Nunito_Sans } from "next/font/google";
+import { PropsWithChildren } from "react";
+import "./globals.css";
+
+const nunitoSans = Nunito_Sans({
+  display: "swap",
+  preload: true,
+  subsets: ["latin"],
+  variable: "--font-nunito-sans",
+  weight: ["400", "700"],
+});
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <body className={clsx(`antialiased`, nunitoSans.className)}>
+        {children}
+      </body>
+    </html>
+  );
+}
