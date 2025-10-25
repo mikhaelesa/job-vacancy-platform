872133d7478d44760d0c48def373333d871e6d5d
mikhaelesa <mikeul2.gt@gmail.com>
2025-10-26 06:01:37 +0700

    (fix): Disable eslint rule due to know issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "react-hooks/refs": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
