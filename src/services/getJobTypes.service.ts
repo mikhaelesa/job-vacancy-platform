8fe961ca7e371256d607f86a08197ad93a2396b8
mikhaelesa <mikeul2.gt@gmail.com>
2025-10-26 02:07:25 +0700

    (fix): Moved getJobTypes.service to `service/jobs` folder

--- src/services/getJobTypes.service.ts
+++
@@ -1,9 +0,0 @@
-import apiClient from "@/src/api/client";
-import { IJobTypesSuccessResponse } from "../dto/jobTypes.dto";
-
-export const getJobTypesService = <T = IJobTypesSuccessResponse>() => {
-  return apiClient.request<T, unknown>({
-    method: "GET",
-    url: "/api/job-types",
-  });
-};
