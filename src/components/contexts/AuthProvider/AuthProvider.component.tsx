81c3b9687cc0e67c8cb0be9fce2279527dd40bae
mikhaelesa <mikeul2.gt@gmail.com>
2025-10-25 21:33:34 +0700

    (feat): Added session to AuthProvider

--- src/components/contexts/AuthProvider/AuthProvider.component.tsx
+++ src/components/contexts/AuthProvider/AuthProvider.component.tsx
@@ -1,21 +1,31 @@
 "use client";
 
 import { supabaseClient } from "@/src/constants/supabaseClient.constant";
-import { User } from "@supabase/supabase-js";
+import { Session, User } from "@supabase/supabase-js";
 import { createContext, PropsWithChildren, useEffect, useState } from "react";
 
 interface IAuthContext {
   user: User | null;
+  isLoading: boolean;
+  session: Session | null;
 }
 
-const AuthContext = createContext<IAuthContext>({ user: null });
+const AuthContext = createContext<IAuthContext>({
+  user: null,
+  isLoading: true,
+  session: null,
+});
 
 const AuthProvider = ({ children }: PropsWithChildren) => {
+  const [session, setSession] = useState<Session | null>(null);
   const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     supabaseClient.auth.getSession().then(({ data }) => {
+      setSession(data.session || null);
       setUser(data.session?.user || null);
+      setIsLoading(false);
     });
 
     const { data } = supabaseClient.auth.onAuthStateChange((_, session) => {
@@ -29,6 +39,8 @@ const AuthProvider = ({ children }: PropsWithChildren) => {
     <AuthContext.Provider
       value={{
         user,
+        session,
+        isLoading,
       }}
     >
       {children}
