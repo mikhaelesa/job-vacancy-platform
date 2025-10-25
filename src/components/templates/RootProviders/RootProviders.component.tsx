b13a30aae73ac6187992830b2d90f75c0b2aef2e
mikhaelesa <mikeul2.gt@gmail.com>
2025-10-26 06:06:24 +0700

    (fix): Wrap client components with Suspense

--- src/components/templates/RootProviders/RootProviders.component.tsx
+++ src/components/templates/RootProviders/RootProviders.component.tsx
@@ -1,12 +1,12 @@
 "use client";
 
+import AuthProvider from "@/src/components/contexts/AuthProvider";
 import { queryClient } from "@/src/constants/queryClient.constant";
 import LoginView from "@/src/features/login/view";
 import SignUpView from "@/src/features/sign-up/view";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, Suspense, useState } from "react";
 import { ToastContainer } from "react-toastify";
-import AuthProvider from "../../contexts/AuthProvider";
 
 const RootProviders = ({ children }: PropsWithChildren) => {
   const [qc] = useState(() => queryClient);
@@ -26,9 +26,11 @@ const RootProviders = ({ children }: PropsWithChildren) => {
       />
       <QueryClientProvider client={qc}>
         <AuthProvider>
-          <SignUpView />
-          <LoginView />
-          {children}
+          <Suspense>
+            <SignUpView />
+            <LoginView />
+            {children}
+          </Suspense>
         </AuthProvider>
       </QueryClientProvider>
     </>
