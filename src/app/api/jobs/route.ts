409e5fa65ed8a7af67e9ec13798a4ee6af31a0fb
mikhaelesa <mikeul2.gt@gmail.com>
2025-10-26 09:21:16 +0700

    (fix): Tidy upt jobs response

--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -25,5 +25,9 @@ export async function GET() {
   if (error)
     return Response.json({ message: error.message, error }, { status: 500 });
 
-  return Response.json({ message: "Success retrieving jobs", data });
+  return Response.json({
+    message: "Success retrieving jobs",
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    data: data.map(({ job_type_id, recruiter_id, city_id, ...rest }) => rest),
+  });
 }
